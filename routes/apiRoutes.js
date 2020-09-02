7a3205ed8e4b9555fab1f4b0f990081405470e3c
pratyu <pratyusha.raghupatruni@gmail.com>
2020-09-01 17:42:59 -0700

    added apiroutes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,26 @@
+var path=require("path");
+const { v4: uuidv4 } = require('uuid');
+
+const notes = require("../db/db.json");
+
+ module.exports=function(app){
+     app.get("/api/notes",function(req,res){
+         res.json(notes);
+     });
+
+     app.post("/api/notes",function(req,res){
+         let newNote=req.body;
+         newNote.id=uuidv4()
+         notes.push(newNote);
+         res.json(newNote);
+     });
+    app.delete("/api/notes/:id",function(req,res){
+        let deleteNote=req.params.id;
+        for(var i=0;i<notes.length;i++){
+            if(deleteNote==notes[i].id){
+                slice(notes[i],1);
+            }
+        }
+        return res.send();
+    })
+ }
