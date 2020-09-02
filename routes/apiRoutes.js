17240456e009b7200fac70b4c25e93b561223472
pratyu <pratyusha.raghupatruni@gmail.com>
2020-09-01 20:27:05 -0700

    delete is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,17 +10,19 @@ const notes = require("../db/db.json");
 
      app.post("/api/notes",function(req,res){
          let newNote=req.body;
-         newNote.id=uuidv4()
+         newNote.id=uuidv4();
+         console.log("new id " + newNote.id);
          notes.push(newNote);
          res.json(newNote);
      });
     app.delete("/api/notes/:id",function(req,res){
         let deleteNote=req.params.id;
+        console.log("delete req " + req.params.id);
         for(var i=0;i<notes.length;i++){
             if(deleteNote==notes[i].id){
-                slice(notes[i],1);
+               notes.splice(i,1);
+              }
             }
-        }
         return res.send();
     });
  }
